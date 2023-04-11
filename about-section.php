ed210714fbfbb1443d2252be6a012c1d9a48a097
Ninaapriy13 <127164505+Ninaapriy13@users.noreply.github.com>
2023-04-11 22:39:40 +0700

    Add files via upload

---
+++ about-section.php
@@ -0,0 +1,43 @@
+<section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>About Me</h2>
+          <p>I am a hardworking and highly motivated student who is currently studying S1 education majoring in Management at Gunung Jati Swadaya University. I am also active in several activities campus internal and external organizations. I am very driven and reliable, when in The new environment will prove me as a competent and highly dedicated worker for a company and for the environment.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="assets/img/profile-img1.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>Management &amp; Web Developer.</h3>
+            <p class="fst-italic">
+            Currently I am studying Marketing Management and also I am taking MSIB Batch 4 in NF Computer for FullStack Web Developer
+            </p>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>13 April 2001</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>https://learn.nurulfikri.com/</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+6283839190756</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Cirebon, Indonesia</span></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>22</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Bachelor degree</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>ninaapriy13@gmail.com</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Not Available</span></li>
+                </ul>
+              </div>
+            </div>
+            <p>
+            "It's fine to celebrate success, but it's more important to heed the lessons of failure." Bill Gates.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section>
---
+++ resume-section.php
@@ -0,0 +1,56 @@
+<section id="resume" class="resume">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Resume</h2>
+          <p>I am a hardworking and highly motivated student who is currently studying S1 education majoring in Management at Gunung Jati Swadaya University. I am also active in several activities campus internal and external organizations. I am very driven and reliable, when in The new environment will prove me as a competent and highly dedicated worker for a company and for the environment.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-up">
+            <h3 class="resume-title">Sumary</h3>
+            <div class="resume-item pb-0">
+              <h4>Nina Apriyanti</h4>
+              <ul>
+                <li><p><em>Receiving Grants for the Student Creativity Program (PKM) Ditjen Belmawa KEMDIKBUDRISTEK 2022</em></p></li>
+                <li><p><em>Passed the MBKM Student Exchange Program held by KEMDIKBUDRISTEK in Makassar public university.</em></p></li>
+              </ul>
+            </div>
+
+            <h3 class="resume-title">Education</h3>
+            <div class="resume-item">
+              <h4>Management Bachelor Degree</h4>
+              <h5>2020 - 2024</h5>
+              <p><em>Gunung Jati Swadaya University, Jawa Barat, Indonesia</em></p>
+              <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
+            </div>
+            <div class="resume-item">
+              <h4>Organisational Experience</h4>
+              <h5>2020 - 2022</h5>
+              <p><em>Gunung Jati Swadaya University, Jawa Barat, Indonesia</em></p>
+              <p>Unit Kegiatan Mahasiswa Penalaran dan Keilmuan, Research and Development Department</p>
+            </div>
+          </div>
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <h3 class="resume-title">External Organizational Experience</h3>
+            <div class="resume-item">
+              <h4>Sarasehan Kemandirian Pangan</h4>
+              <h5>2022 - Present</h5>
+              <p><em>Cirebon, Jawa Barat, Indonesia </em></p>
+              <ul>
+                <li>Collaborating with all aspects of Pentahelix in Cirebon, Indramayu, Majalengka, and Kuningan. This event was organized by ICMI (Ikatan Cendekiawan Muslim Indonesia) with the theme "Integrated Farming With The Pentahelix Collaborative Model for Food Self-Sufficiency in Ciayumajakuning".</li>
+              </ul>
+            </div>
+            <div class="resume-item">
+              <h4>Webinars Attended</h4>
+              <h5>2021</h5>
+              <p><em>Cirebon, Jawa Barat, Indonesia</em></p>
+              <ul>
+                <li>MC in the Webinar and Public Discussion event in the UKM P&K 2021 Scientific Lively activity</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
